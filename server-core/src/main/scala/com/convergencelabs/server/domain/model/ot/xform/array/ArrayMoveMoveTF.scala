b96f3d8fd73b3b2cf1645d6cc8a2f9647a0bdc35
Michael MacFadden <michael@convergencelabs.com>
2015-12-21 23:49:44 -0700

    Move move testing.

--- server-core/src/main/scala/com/convergencelabs/server/domain/model/ot/xform/array/ArrayMoveMoveTF.scala
+++ server-core/src/main/scala/com/convergencelabs/server/domain/model/ot/xform/array/ArrayMoveMoveTF.scala
@@ -12,8 +12,8 @@ private[ot] object ArrayMoveMoveTF extends OperationTransformationFunction[Array
     (sMoveType, cMoveType) match {
       case (Forward, Forward) => transformTwoForwardMoves(s, c)
       case (Forward, Backward) => transofrmServerForwardMoveWithClientBackwardMove(s, c)
-      case (Backward, Forward) => transofrmBackwardMoveWithForwardMove(s, c)
-      case (Backward, Backward) => transofrmBackwardMoveWithBackwardMove(s, c)
+      case (Backward, Forward) => transformServerBackwardMoveWithClientForwardMove(s, c)
+      case (Backward, Backward) => transformServerBackwardMoveWithClientBackwardMove(s, c)
       case (Identity, _) => (s, c)
       case (_, Identity) => (s, c)
     }
@@ -21,85 +21,179 @@ private[ot] object ArrayMoveMoveTF extends OperationTransformationFunction[Array
 
   def transformTwoForwardMoves(s: ArrayMoveOperation, c: ArrayMoveOperation): (ArrayMoveOperation, ArrayMoveOperation) = {
     ArrayMoveRangeHelper.getRangeRelationship(s, c) match {
-      case Precedes | PrecededBy =>
+      case Precedes =>
+        // A-MM-FF-1
         (s, c)
-      case MetBy | OverlappedBy | Finishes =>
-        (s.copy(fromIndex = s.fromIndex - 1), c.copy(toIndex = c.toIndex - 1))
-      case ContainedBy =>
-        (s.copy(fromIndex = s.fromIndex - 1, toIndex = s.toIndex - 1), c)
-      case Starts | StartedBy =>
-        (s.copy(fromIndex = c.toIndex), c.copy(noOp = true))
-      case Contains | FinishedBy =>
-        (s, c.copy(fromIndex = c.fromIndex - 1, toIndex = c.toIndex - 1))
-      case Overlaps | Meets =>
-        (s.copy(toIndex = s.toIndex - 1), c.copy(fromIndex = c.fromIndex - 1))
-      case EqualTo =>
-        (s.copy(noOp = true), c.copy(noOp = true))
-    }
-  }
-
-  def transofrmBackwardMoveWithBackwardMove(s: ArrayMoveOperation, c: ArrayMoveOperation): (ArrayMoveOperation, ArrayMoveOperation) = {
-    ArrayMoveRangeHelper.getRangeRelationship(s, c) match {
-      case Precedes | PrecededBy =>
-        (s, c)
-      case Meets | Overlaps =>
-        (s.copy(toIndex = s.toIndex + 1), c.copy(fromIndex = c.fromIndex + 1))
-      case FinishedBy | Finishes =>
-        (s.copy(fromIndex = c.toIndex), c.copy(noOp = true))
-      case Contains =>
-        (s.copy(fromIndex = s.fromIndex + 1, toIndex = s.toIndex + 1), c)
-      case Starts | ContainedBy =>
-        (s, c.copy(fromIndex = c.fromIndex + 1, toIndex = c.toIndex + 1))
-      case EqualTo =>
-        (s.copy(noOp = true), c.copy(noOp = true))
-      case StartedBy | OverlappedBy | MetBy =>
-        (s.copy(fromIndex = s.fromIndex + 1), c.copy(toIndex = c.toIndex + 1))
-    }
-  }
-
-  def transofrmBackwardMoveWithForwardMove(s: ArrayMoveOperation, c: ArrayMoveOperation): (ArrayMoveOperation, ArrayMoveOperation) = {
-    ArrayMoveRangeHelper.getRangeRelationship(s, c) match {
-      case Precedes | PrecededBy =>
+      case PrecededBy =>
+        // A-MM-FF-2
         (s, c)
       case Meets =>
-        (s.copy(fromIndex = c.toIndex), c.copy(noOp = true))
-      case FinishedBy | Starts | EqualTo =>
-        (s.copy(fromIndex = s.fromIndex + 1), c.copy(fromIndex = c.fromIndex - 1))
-      case Contains | StartedBy =>
-        (s.copy(fromIndex = s.fromIndex + 1, toIndex = s.toIndex + 1), c)
-      case ContainedBy | Finishes =>
-        (s, c.copy(fromIndex = c.fromIndex + 1, toIndex = c.toIndex + 1))
+        // A-MM-FF-3
+        (s.copy(toIndex = s.toIndex - 1), c.copy(fromIndex = c.fromIndex - 1))
+      case MetBy =>
+        // A-MM-FF-4
+        (s.copy(fromIndex = s.fromIndex - 1), c.copy(toIndex = c.toIndex - 1))
+      case Overlaps =>
+        // A-MM-FF-5
+        (s.copy(toIndex = s.toIndex - 1), c.copy(fromIndex = c.fromIndex - 1))
       case OverlappedBy =>
-        (s.copy(toIndex = s.toIndex + 1), c.copy(toIndex = c.toIndex - 1))
+        // A-MM-FF-6
+        (s.copy(fromIndex = s.fromIndex - 1), c.copy(toIndex = c.toIndex - 1))
+      case Starts =>
+        // A-MM-FF-7
+        (s.copy(fromIndex = c.toIndex), c.copy(noOp = true))
+      case StartedBy =>
+        // A-MM-FF-8
+        (s.copy(fromIndex = c.toIndex), c.copy(noOp = true))
+      case Contains =>
+        // A-MM-FF-9
+        (s, c.copy(fromIndex = c.fromIndex - 1, toIndex = c.toIndex - 1))
+      case ContainedBy =>
+        // A-MM-FF-10
+        (s.copy(fromIndex = s.fromIndex - 1, toIndex = s.toIndex - 1), c)
+      case Finishes =>
+        // A-MM-FF-11
+        (s.copy(fromIndex = s.fromIndex - 1), c.copy(toIndex = c.toIndex - 1))
+      case FinishedBy =>
+        // A-MM-FF-12
+        (s, c.copy(fromIndex = c.fromIndex - 1, toIndex = c.toIndex - 1))
+      case EqualTo =>
+        // A-MM-FF-13
+        (s.copy(noOp = true), c.copy(noOp = true))
     }
   }
 
   def transofrmServerForwardMoveWithClientBackwardMove(s: ArrayMoveOperation, c: ArrayMoveOperation): (ArrayMoveOperation, ArrayMoveOperation) = {
     ArrayMoveRangeHelper.getRangeRelationship(s, c) match {
-      case Precedes | PrecededBy =>
+      case Precedes =>
+        // A-MM-FB-1
         (s, c)
-      case Meets => //Broken
+      case PrecededBy =>
+        // A-MM-FB-2
+        (s, c)
+      case Meets =>
+        // A-MM-FB-3
+        // FIXME Broken
         (s.copy(toIndex = s.toIndex - 1), c.copy(toIndex = c.toIndex + 1))
       case MetBy =>
+        // A-MM-FB-4
         (s.copy(fromIndex = c.toIndex), c.copy(noOp = true))
       case Overlaps =>
+        // A-MM-FB-5
         (s.copy(toIndex = s.toIndex + 1), c.copy(toIndex = c.toIndex - 1))
       case OverlappedBy =>
+        // A-MM-FB-6
         (s.copy(fromIndex = s.fromIndex + 1), c.copy(fromIndex = c.fromIndex - 1))
       case Starts =>
+        // A-MM-FB-7
         (s.copy(fromIndex = s.fromIndex + 1, toIndex = s.toIndex + 1), c)
       case StartedBy =>
+        // A-MM-FB-8
         (s.copy(fromIndex = s.fromIndex + 1), c.copy(fromIndex = c.fromIndex - 1))
       case Contains =>
+        // A-MM-FB-9
         (s, c.copy(fromIndex = c.fromIndex - 1, toIndex = c.toIndex - 1))
       case ContainedBy =>
+        // A-MM-FB-10
         (s.copy(fromIndex = s.fromIndex + 1, toIndex = s.toIndex + 1), c)
       case Finishes =>
+        // A-MM-FB-11
         (s.copy(fromIndex = s.fromIndex + 1), c.copy(fromIndex = c.fromIndex - 1))
       case FinishedBy =>
+        // A-MM-FB-12
         (s, c.copy(fromIndex = c.fromIndex - 1, toIndex = c.toIndex - 1))
       case EqualTo =>
+        // A-MM-FB-13
         (s.copy(fromIndex = s.fromIndex + 1), c.copy(fromIndex = c.fromIndex - 1))
+    }
+  }
+
+  def transformServerBackwardMoveWithClientForwardMove(s: ArrayMoveOperation, c: ArrayMoveOperation): (ArrayMoveOperation, ArrayMoveOperation) = {
+    ArrayMoveRangeHelper.getRangeRelationship(s, c) match {
+      case Precedes =>
+        // A-MM-BF-1
+        (s, c)
+      case PrecededBy =>
+        // A-MM-BF-2
+        (s, c)
+      case Meets =>
+        // A-MM-BF-3
+        (s.copy(fromIndex = c.toIndex), c.copy(noOp = true))
+      case MetBy =>
+        // A-MM-BF-4
+        // FIXME Broken
+        (s, c)
+      case Overlaps =>
+        // A-MM-BF-5
+        (s.copy(fromIndex = s.fromIndex - 1), c.copy(fromIndex = c.fromIndex + 1))
+      case OverlappedBy =>
+        // A-MM-BF-6
+        (s.copy(toIndex = s.toIndex - 1), c.copy(toIndex = c.toIndex + 1))
+      case Starts =>
+        // A-MM-BF-7
+        (s.copy(fromIndex = s.fromIndex - 1), c.copy(fromIndex = c.fromIndex + 1))
+      case StartedBy =>
+        // A-MM-BF-8
+        (s, c.copy(fromIndex = c.fromIndex + 1, toIndex = c.toIndex + 1))
+      case Contains =>
+        // A-MM-BF-9
+        (s, c.copy(fromIndex = c.fromIndex + 1, toIndex = c.toIndex + 1))
+      case ContainedBy =>
+        // A-MM-BF-10
+        (s.copy(fromIndex = s.fromIndex - 1, toIndex = s.toIndex - 1), c)
+      case Finishes =>
+        // A-MM-BF-11
+        (s.copy(fromIndex = s.fromIndex - 1, toIndex = s.toIndex - 1), c)
+      case FinishedBy =>
+        // A-MM-BF-12
+        (s.copy(fromIndex = s.fromIndex - 1), c.copy(fromIndex = c.fromIndex + 1))
+      case EqualTo =>
+        // A-MM-BF-13
+        (s.copy(fromIndex = s.fromIndex - 1), c.copy(fromIndex = c.fromIndex + 1))
+    }
+  }
+
+  def transformServerBackwardMoveWithClientBackwardMove(s: ArrayMoveOperation, c: ArrayMoveOperation): (ArrayMoveOperation, ArrayMoveOperation) = {
+    ArrayMoveRangeHelper.getRangeRelationship(s, c) match {
+      case Precedes =>
+        // A-MM-BB-1
+        (s, c)
+      case PrecededBy =>
+        // A-MM-BB-2
+        (s, c)
+      case Meets =>
+        // A-MM-BB-3
+        (s.copy(fromIndex = s.fromIndex + 1), c.copy(toIndex = c.toIndex + 1))
+      case MetBy =>
+        // A-MM-BB-4
+        (s.copy(toIndex = s.toIndex + 1), c.copy(fromIndex = c.fromIndex + 1))
+      case Overlaps =>
+        // A-MM-BB-5
+        (s.copy(fromIndex = s.fromIndex + 1), c.copy(toIndex = c.toIndex + 1))
+      case OverlappedBy =>
+        // A-MM-BB-6
+        (s.copy(toIndex = s.toIndex + 1), c.copy(fromIndex = c.fromIndex + 1))
+      case Starts =>
+        // A-MM-BB-7
+        (s.copy(fromIndex = s.fromIndex + 1, toIndex = s.toIndex + 1), c)
+      case StartedBy =>
+        // A-MM-BB-8
+        (s, c.copy(fromIndex = c.fromIndex + 1, toIndex = c.toIndex + 1))
+      case Contains =>
+        // A-MM-BB-9
+        (s, c.copy(fromIndex = c.fromIndex + 1, toIndex = c.toIndex + 1))
+      case ContainedBy =>
+        // A-MM-BB-10
+        (s.copy(fromIndex = s.fromIndex + 1, toIndex = s.toIndex + 1), c)
+      case Finishes =>
+        // A-MM-BB-11
+        (s.copy(fromIndex = c.toIndex), c.copy(noOp = true))
+      case FinishedBy =>
+        // A-MM-BB-12
+        (s.copy(fromIndex = c.toIndex), c.copy(noOp = true))
+      case EqualTo =>
+        // A-MM-BB-13
+        (s.copy(noOp = true), c.copy(noOp = true))
     }
   }
 }
--- server-core/src/test/scala/com/convergencelabs/server/domain/model/ot/xform/ArrayMoveMoveTFSpec.scala
+++ server-core/src/test/scala/com/convergencelabs/server/domain/model/ot/xform/ArrayMoveMoveTFSpec.scala
@@ -355,12 +355,347 @@ class ArrayMoveMoveTFSpec
       }
     }
 
-    "tranforming a backward move against a forward move" must {
+    "tranforming a server backward move against a client forward move" must {
+      /**
+       * A-MM-BF-1 - Server Backward Move Precedes Client Forward Move
+       */
+      "not transform either operation when the server's move preceeds the client's move" in {
+        val s = ArrayMoveOperation(Path, false, 4, 2)
+        val c = ArrayMoveOperation(Path, false, 5, 7)
 
+        val (s1, c1) = ArrayMoveMoveTF.transform(s, c)
+
+        s1 shouldBe s
+        c1 shouldBe c
+      }
+
+      /**
+       * A-MM-BF-2 - Server Backward Move Preceded By Client Forward Move
+       */
+      "not transform either operation when the server's move is preceded by the client's move" in {
+        val s = ArrayMoveOperation(Path, false, 7, 5)
+        val c = ArrayMoveOperation(Path, false, 2, 4)
+
+        val (s1, c1) = ArrayMoveMoveTF.transform(s, c)
+
+        s1 shouldBe s
+        c1 shouldBe c
+      }
+
+      /**
+       * A-MM-FB-3 - Server Backward Move Meets Client Forward Move
+       */
+      "decrement the server move's toIndex and decrement the client move's fromIndex when the server move meets the client move" in {
+        val s = ArrayMoveOperation(Path, false, 5, 3)
+        val c = ArrayMoveOperation(Path, false, 5, 7)
+
+        val (s1, c1) = ArrayMoveMoveTF.transform(s, c)
+
+        s1 shouldBe ArrayMoveOperation(Path, false, 7, 3)
+        c1 shouldBe ArrayMoveOperation(Path, true, 5, 7)
+      }
+
+      /**
+       * A-MM-BF-4 - Server Backward Move Met By Client Forward Move
+       */
+      "set the server move's fromIndex to the client move's toIndex and noOp the client move, when the server move is met by the client move" in {
+        // FIXME this is a broken case.
+        val s = ArrayMoveOperation(Path, false, 7, 5)
+        val c = ArrayMoveOperation(Path, false, 3, 5)
+
+        val (s1, c1) = ArrayMoveMoveTF.transform(s, c)
+
+        s1 shouldBe ArrayMoveOperation(Path, false, 3, 7)
+        c1 shouldBe ArrayMoveOperation(Path, true, 5, 3)
+      }
+
+      /**
+       * A-MM-BF-5 - Server Backward Move Overlaps Client Forward Move
+       */
+      "increment the server move's toIndex and decrement the client move's toIndex, when the server move overlaps the client move" in {
+        val s = ArrayMoveOperation(Path, false, 5, 3)
+        val c = ArrayMoveOperation(Path, false, 4, 6)
+
+        val (s1, c1) = ArrayMoveMoveTF.transform(s, c)
+
+        s1 shouldBe ArrayMoveOperation(Path, false, 4, 3)
+        c1 shouldBe ArrayMoveOperation(Path, false, 5, 6)
+      }
+
+      /**
+       * A-MM-BF-6 - Server Backward Move Overlapped By Client Forward Move
+       */
+      "increment the server move's fromIndex and decrement the client move's fromIndex, when the server move is overlapped by the client move" in {
+        val s = ArrayMoveOperation(Path, false, 6, 4)
+        val c = ArrayMoveOperation(Path, false, 3, 5)
+
+        val (s1, c1) = ArrayMoveMoveTF.transform(s, c)
+
+        s1 shouldBe ArrayMoveOperation(Path, false, 6, 3)
+        c1 shouldBe ArrayMoveOperation(Path, false, 3, 6)
+      }
+
+      /**
+       * A-MM-BF-7 - Server Backward Move Starts Client Forward Move
+       */
+      "increment the server move's fromIndex and toIndex and not transform the client move, when the server move starts the client move" in {
+        val s = ArrayMoveOperation(Path, false, 5, 3)
+        val c = ArrayMoveOperation(Path, false, 3, 6)
+
+        val (s1, c1) = ArrayMoveMoveTF.transform(s, c)
+
+        s1 shouldBe ArrayMoveOperation(Path, false, 4, 3)
+        c1 shouldBe ArrayMoveOperation(Path, false, 4, 6)
+      }
+
+      /**
+       * A-MM-BF-8 - Server Backward Move Started By Client Forward Move
+       */
+      "increment the server move's fromIndex and decrement the client move's fromInex, when the server move is started by the client move" in {
+        val s = ArrayMoveOperation(Path, false, 6, 3)
+        val c = ArrayMoveOperation(Path, false, 3, 5)
+
+        val (s1, c1) = ArrayMoveMoveTF.transform(s, c)
+
+        s1 shouldBe ArrayMoveOperation(Path, false, 6, 3)
+        c1 shouldBe ArrayMoveOperation(Path, false, 4, 6)
+      }
+
+      /**
+       * A-MM-BF-9 - Server Backward Move Contains Client Forward Move
+       */
+      "not transform the server move and decrement the client move's fromIndex and toIndex, when the server move contains the client move" in {
+        val s = ArrayMoveOperation(Path, false, 6, 3)
+        val c = ArrayMoveOperation(Path, false, 4, 5)
+
+        val (s1, c1) = ArrayMoveMoveTF.transform(s, c)
+
+        s1 shouldBe s
+        c1 shouldBe ArrayMoveOperation(Path, false, 5, 6)
+      }
+
+      /**
+       * A-MM-BF-10 - Server Backward Move Contained By Client Forward Move
+       */
+      "increment the server move's fromIndex and toIndex and not transform the client move, when the server move is contained by the client move" in {
+        val s = ArrayMoveOperation(Path, false, 5, 4)
+        val c = ArrayMoveOperation(Path, false, 3, 6)
+
+        val (s1, c1) = ArrayMoveMoveTF.transform(s, c)
+
+        s1 shouldBe ArrayMoveOperation(Path, false, 4, 3)
+        c1 shouldBe c
+      }
+
+      /**
+       * A-MM-BF-11 - Server Backward Move Finishes Client Forward Move
+       */
+      "decrement the server move's fromIndex and the client move's fromIndex, when the server move finishes the client move" in {
+        val s = ArrayMoveOperation(Path, false, 6, 4)
+        val c = ArrayMoveOperation(Path, false, 2, 6)
+
+        val (s1, c1) = ArrayMoveMoveTF.transform(s, c)
+
+        s1 shouldBe ArrayMoveOperation(Path, false, 5, 3)
+        c1 shouldBe c
+      }
+
+      /**
+       * A-MM-BF-12 - Server Backward Move Finished By Client Forward Move
+       */
+      "not transform the server move and decrement the client move's fromIndex and toIndex, when the server move finishes the client move" in {
+        val s = ArrayMoveOperation(Path, false, 6, 2)
+        val c = ArrayMoveOperation(Path, false, 4, 6)
+
+        val (s1, c1) = ArrayMoveMoveTF.transform(s, c)
+
+        s1 shouldBe ArrayMoveOperation(Path, false, 5, 2)
+        c1 shouldBe ArrayMoveOperation(Path, false, 5, 6)
+      }
+
+      /**
+       * A-MM-BF-13 - Server Backward Move Equal To Client Forward Move
+       */
+      "increment the server move's fromIndex and decrement the client move's fromIndex, when the server move is equal to the client move" in {
+        val s = ArrayMoveOperation(Path, false, 6, 2)
+        val c = ArrayMoveOperation(Path, false, 2, 6)
+
+        val (s1, c1) = ArrayMoveMoveTF.transform(s, c)
+
+        s1 shouldBe ArrayMoveOperation(Path, false, 5, 2)
+        c1 shouldBe ArrayMoveOperation(Path, false, 3, 6)
+      }
     }
 
-    "tranforming a backward move against a backward move" must {
+    "tranforming a server backward move against a client backward move" must {
+      /**
+       * A-MM-BB-1 - Server Backward Move Precedes Client Backward Move
+       */
+      "not transform either operation when the server's move preceeds the client's move" in {
+        val s = ArrayMoveOperation(Path, false, 4, 2)
+        val c = ArrayMoveOperation(Path, false, 7, 5)
 
+        val (s1, c1) = ArrayMoveMoveTF.transform(s, c)
+
+        s1 shouldBe s
+        c1 shouldBe c
+      }
+
+      /**
+       * A-MM-BB-2 - Server Backward Move Preceded By Client Backward Move
+       */
+      "not transform either operation when the server's move is preceded by the client's move" in {
+        val s = ArrayMoveOperation(Path, false, 7, 5)
+        val c = ArrayMoveOperation(Path, false, 4, 2)
+
+        val (s1, c1) = ArrayMoveMoveTF.transform(s, c)
+
+        s1 shouldBe s
+        c1 shouldBe c
+      }
+
+      /**
+       * A-MM-BB-3 - Server Backward Move Meets Client Backward Move
+       */
+      "decrement the server move's toIndex and decrement the client move's fromIndex when the server move meets the client move" in {
+        val s = ArrayMoveOperation(Path, false, 5, 3)
+        val c = ArrayMoveOperation(Path, false, 7, 5)
+
+        val (s1, c1) = ArrayMoveMoveTF.transform(s, c)
+
+        s1 shouldBe ArrayMoveOperation(Path, false, 6, 3)
+        c1 shouldBe ArrayMoveOperation(Path, false, 7, 6)
+      }
+
+      /**
+       * A-MM-BB-4 - Server Backward Move Met By Client Backward Move
+       */
+      "set the server move's fromIndex to the client move's toIndex and noOp the client move, when the server move is met by the client move" in {
+        val s = ArrayMoveOperation(Path, false, 7, 5)
+        val c = ArrayMoveOperation(Path, false, 5, 3)
+
+        val (s1, c1) = ArrayMoveMoveTF.transform(s, c)
+
+        s1 shouldBe ArrayMoveOperation(Path, false, 7, 6)
+        c1 shouldBe ArrayMoveOperation(Path, false, 6, 3)
+      }
+
+      /**
+       * A-MM-BB-5 - Server Backward Move Overlaps Client Backward Move
+       */
+      "increment the server move's toIndex and decrement the client move's toIndex, when the server move overlaps the client move" in {
+        val s = ArrayMoveOperation(Path, false, 5, 3)
+        val c = ArrayMoveOperation(Path, false, 6, 4)
+
+        val (s1, c1) = ArrayMoveMoveTF.transform(s, c)
+
+        s1 shouldBe ArrayMoveOperation(Path, false, 6, 3)
+        c1 shouldBe ArrayMoveOperation(Path, false, 6, 5)
+      }
+
+      /**
+       * A-MM-BB-6 - Server Backward Move Overlapped By Client Backward Move
+       */
+      "increment the server move's fromIndex and decrement the client move's fromIndex, when the server move is overlapped by the client move" in {
+        val s = ArrayMoveOperation(Path, false, 6, 4)
+        val c = ArrayMoveOperation(Path, false, 5, 3)
+
+        val (s1, c1) = ArrayMoveMoveTF.transform(s, c)
+
+        s1 shouldBe ArrayMoveOperation(Path, false, 6, 5)
+        c1 shouldBe ArrayMoveOperation(Path, false, 6, 3)
+      }
+
+      /**
+       * A-MM-BB-7 - Server Backward Move Starts Client Backward Move
+       */
+      "increment the server move's fromIndex and toIndex and not transform the client move, when the server move starts the client move" in {
+        val s = ArrayMoveOperation(Path, false, 5, 3)
+        val c = ArrayMoveOperation(Path, false, 6, 3)
+
+        val (s1, c1) = ArrayMoveMoveTF.transform(s, c)
+
+        s1 shouldBe ArrayMoveOperation(Path, false, 6, 4)
+        c1 shouldBe ArrayMoveOperation(Path, false, 6, 3)
+      }
+
+      /**
+       * A-MM-BB-8 - Server Backward Move Started By Client Backward Move
+       */
+      "increment the server move's fromIndex and decrement the client move's fromInex, when the server move is started by the client move" in {
+        val s = ArrayMoveOperation(Path, false, 6, 3)
+        val c = ArrayMoveOperation(Path, false, 5, 3)
+
+        val (s1, c1) = ArrayMoveMoveTF.transform(s, c)
+
+        s1 shouldBe ArrayMoveOperation(Path, false, 6, 3)
+        c1 shouldBe ArrayMoveOperation(Path, false, 6, 4)
+      }
+
+      /**
+       * A-MM-BB-9 - Server Backward Move Contains Client Backward Move
+       */
+      "not transform the server move and decrement the client move's fromIndex and toIndex, when the server move contains the client move" in {
+        val s = ArrayMoveOperation(Path, false, 6, 3)
+        val c = ArrayMoveOperation(Path, false, 5, 4)
+
+        val (s1, c1) = ArrayMoveMoveTF.transform(s, c)
+
+        s1 shouldBe s
+        c1 shouldBe ArrayMoveOperation(Path, false, 6, 5)
+      }
+
+      /**
+       * A-MM-BB-10 - Server Backward Move Contained By Client Backward Move
+       */
+      "increment the server move's fromIndex and toIndex and not transform the client move, when the server move is contained by the client move" in {
+        val s = ArrayMoveOperation(Path, false, 5, 4)
+        val c = ArrayMoveOperation(Path, false, 6, 3)
+
+        val (s1, c1) = ArrayMoveMoveTF.transform(s, c)
+
+        s1 shouldBe ArrayMoveOperation(Path, false, 6, 5)
+        c1 shouldBe c
+      }
+
+      /**
+       * A-MM-BF-11 - Server Backward Move Finishes Client Backward Move
+       */
+      "decrement the server move's fromIndex and the client move's fromIndex, when the server move finishes the client move" in {
+        val s = ArrayMoveOperation(Path, false, 6, 4)
+        val c = ArrayMoveOperation(Path, false, 6, 2)
+
+        val (s1, c1) = ArrayMoveMoveTF.transform(s, c)
+
+        s1 shouldBe ArrayMoveOperation(Path, false, 2, 4)
+        c1 shouldBe ArrayMoveOperation(Path, true, 6, 2)
+      }
+
+      /**
+       * A-MM-BB-12 - Server Backward Move Finished By Client Backward Move
+       */
+      "not transform the server move and decrement the client move's fromIndex and toIndex, when the server move finishes the client move" in {
+        val s = ArrayMoveOperation(Path, false, 6, 2)
+        val c = ArrayMoveOperation(Path, false, 6, 4)
+
+        val (s1, c1) = ArrayMoveMoveTF.transform(s, c)
+
+        s1 shouldBe ArrayMoveOperation(Path, false, 4, 2)
+        c1 shouldBe ArrayMoveOperation(Path, true, 6, 4)
+      }
+
+      /**
+       * A-MM-BB-13 - Server Backward Move Equal To Client Backward Move
+       */
+      "increment the server move's fromIndex and decrement the client move's fromIndex, when the server move is equal to the client move" in {
+        val s = ArrayMoveOperation(Path, false, 6, 2)
+        val c = ArrayMoveOperation(Path, false, 6, 2)
+
+        val (s1, c1) = ArrayMoveMoveTF.transform(s, c)
+
+        s1 shouldBe ArrayMoveOperation(Path, true, 6, 2)
+        c1 shouldBe ArrayMoveOperation(Path, true, 6, 2)
+      }
     }
   }
 }
