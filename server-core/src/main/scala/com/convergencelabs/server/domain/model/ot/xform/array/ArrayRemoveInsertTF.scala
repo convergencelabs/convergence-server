642bcb03b5fdf08d029e74bbb1dab516f89958b8
Michael MacFadden <michael@convergencelabs.com>
2015-12-24 20:30:17 -0700

    Fixed a but in the ArrayRemoveRemoveTF

--- server-core/src/main/scala/com/convergencelabs/server/domain/model/ot/xform/array/ArrayRemoveInsertTF.scala
+++ server-core/src/main/scala/com/convergencelabs/server/domain/model/ot/xform/array/ArrayRemoveInsertTF.scala
@@ -2,7 +2,7 @@ package com.convergencelabs.server.domain.model.ot
 
 private[ot] object ArrayRemoveInsertTF extends OperationTransformationFunction[ArrayRemoveOperation, ArrayInsertOperation] {
   def transform(s: ArrayRemoveOperation, c: ArrayInsertOperation): (ArrayRemoveOperation, ArrayInsertOperation) = {
-    if (s.index <= c.index) {
+    if (s.index < c.index) {
       (s, c.copy(index = c.index - 1))
     } else {
       (s.copy(index = s.index + 1), c)
