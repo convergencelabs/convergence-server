ce06582a24577c4a410d3be3f1cbbafc316f0dc8
Michael MacFadden <michael@convergencelabs.com>
2016-01-10 12:29:55 -0800

    Additional OTF Test Harness work.

--- server-testkit/src/main/scala/com/convergencelabs/server/domain/model/ot/OTFTestCase.scala
+++ server-testkit/src/main/scala/com/convergencelabs/server/domain/model/ot/OTFTestCase.scala
@@ -2,5 +2,7 @@ package com.convergencelabs.server.domain.model.ot
 
 import org.json4s.JsonAST.JObject
 
+
 case class OperationPair(serverOp: JObject, clientOp: JObject)
 case class OTFTestCase(id: String, input: OperationPair, output: Option[OperationPair], error: Option[Boolean])
+case class OTFSpec(serverOpType: String, clientOpType: String, cases: List[OTFTestCase])
--- server-testkit/src/main/scala/com/convergencelabs/server/domain/model/ot/OTFTestHarness.scala
+++ server-testkit/src/main/scala/com/convergencelabs/server/domain/model/ot/OTFTestHarness.scala
@@ -5,104 +5,73 @@ import java.nio.file.Files
 import java.nio.file.Paths
 import java.time.Duration
 import java.time.Instant
+
 import scala.language.implicitConversions
 import scala.util.Failure
 import scala.util.Success
 import scala.util.Try
+
 import org.json4s.DefaultFormats
 import org.json4s.JArray
 import org.json4s.JBool
 import org.json4s.JInt
 import org.json4s.JString
+import org.json4s.JsonAST.JNumber
 import org.json4s.JsonAST.JObject
 import org.json4s.jackson.Serialization.read
-import org.json4s.JsonAST.JNumber
+import org.scalatest.Finders
+import org.scalatest.FunSpec
 
-object OTFTestHarness extends App {
+class OTFTestHarness extends FunSpec {
 
   val commonPath = List()
 
   val registry = new TransformationFunctionRegistry()
   implicit val format = DefaultFormats
 
-  val startTime = Instant.now()
-
-  var totalTests = 0
-  var passedTests = 0
-  var failedTests = 0
-  var erroredTests = 0
-
   for (file <- new File("src/test/otfspec").listFiles.filter { x => x.getName.endsWith("spec.json") }) {
     processFile(file)
   }
 
-  printSummary()
-
   def processFile(specFile: File): Unit = {
-    println(s"\nProcessing file: ${specFile.getName}")
     val contents = new String(Files.readAllBytes(Paths.get(specFile.getAbsolutePath)))
-    val cases = read[List[OTFTestCase]](contents)
+    val spec = read[OTFSpec](contents)
 
-    for (testCase <- cases) {
-      totalTests += 1
+    describe(s"Testing transformation of a server ${spec.serverOpType} and a client ${spec.clientOpType}") {
+      for (testCase <- spec.cases) {
+        val JString(testCaseServerType) = testCase.input.serverOp \\ "type"
+        val JString(testCaseClientType) = testCase.input.clientOp \\ "type"
 
-      Try(processCase(testCase)) match {
-        case Failure(cause) =>
-          erroredTests += 1
-          println(s"error")
-          cause.printStackTrace()
-        case _ =>
+        if (spec.serverOpType != testCaseServerType || spec.clientOpType != testCaseClientType) {
+          throw new IllegalArgumentException(
+            s"The spec file contains an invalid test case, because the input operations are of the wrong type:\n${specFile.getAbsolutePath}")
+        }
+
+        processCase(testCase)
       }
     }
   }
 
-  def printSummary(): Unit = {
-    val endTime = Instant.now()
-    val duration = Duration.between(startTime, endTime)
-
-    val minutes = Math.floor(duration.getSeconds / 60).asInstanceOf[Int]
-    val seconds = duration.getSeconds % 60
-    val millis = duration.getNano / 1000000
-
-    println(s"\nTest completed in $minutes minute(s), $seconds second(s), $millis millisecond(s)")
-    println(s"Total number of tests run: $totalTests")
-    println(s"Tests: passed $passedTests, failed $failedTests, errored $erroredTests")
-  }
-
   def processCase(testCase: OTFTestCase): Unit = {
-    print(s"Executing test ${testCase.id}: ")
-
     val originalServerOp: DiscreteOperation = testCase.input.serverOp
     val originalClientOp: DiscreteOperation = testCase.input.clientOp
 
-    val tf = registry.getTransformationFunction(originalServerOp, originalClientOp).get
+    it(s"Testing transformation of ${originalServerOp} and a client ${originalClientOp}") {
+      val tf = registry.getTransformationFunction(originalServerOp, originalClientOp).get
 
-    (testCase.error, Try(tf.transform(originalServerOp, originalClientOp))) match {
-      case (Some(true), Success(_)) =>
-        failedTests += 1
-        println(s"failed")
-        println(s"  Expected error condition, but the transformation succeeded.")
-      case (_, Success((sPrime, cPrime))) =>
-        val expectedServerOp: DiscreteOperation = testCase.output.get.serverOp
-        val expectedClientOp: DiscreteOperation = testCase.output.get.clientOp
+      testCase.error match {
+        case Some(true) =>
+          intercept[IllegalArgumentException] {
+            tf.transform(originalServerOp, originalClientOp)
+          }
+        case _ =>
+          val (sPrime, cPrime) = tf.transform(originalServerOp, originalClientOp)
+          val expectedServerOp: DiscreteOperation = testCase.output.get.serverOp
+          val expectedClientOp: DiscreteOperation = testCase.output.get.clientOp
 
-        if (sPrime != expectedServerOp || cPrime != expectedClientOp) {
-          failedTests += 1
-          println(s"failed")
-          println(s"  Expected Server Op: $expectedServerOp")
-          println(s"  Produced Server Op: $sPrime\n")
-
-          println(s"  Expected Client Op: $expectedClientOp")
-          println(s"  Produced Client Op: $cPrime")
-        } else {
-          passedTests += 1
-          println(s"passed")
-        }
-      case (Some(true), Failure(cause)) =>
-        passedTests += 1
-        println(s"passed")
-      case (_, Failure(cause)) =>
-        throw cause
+          assert(sPrime == expectedServerOp, "server operation was transformed incorrectly")
+          assert(cPrime == expectedClientOp, "server operation was transformed incorrectly")
+      }
     }
   }
 
