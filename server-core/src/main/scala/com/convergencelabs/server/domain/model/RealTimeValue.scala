35898f1195bbcdb1c86b8ad578b6f0f50910f60e
Michael MacFadden <michael@convergencelabs.com>
2016-03-24 14:56:34 -0600

    Minor tweaks to real time value.

--- server-core/src/main/scala/com/convergencelabs/server/domain/model/RealTimeValue.scala
+++ server-core/src/main/scala/com/convergencelabs/server/domain/model/RealTimeValue.scala
@@ -13,36 +13,38 @@ abstract class RealTimeValue(
     private[model] var parent: Option[RealTimeContainerValue],
     private[model] var parentField: Option[Any],
     validReferenceTypes: List[ReferenceType.Value]) {
-  
+
   model.registerValue(this)
-  
+
   protected val referenceManager = new ReferenceManager(this, validReferenceTypes)
-  
+
   def path(): List[Any] = {
     parent match {
       case None => List()
       case Some(p) => p.path() :+ parentField
     }
   }
-  
+
   def detach(): Unit = {
     model.unregisterValue(this)
   }
-  
+
   def data(): Any
-  
-  
+
   def processOperation(operation: DiscreteOperation): Try[Unit]
-  
+
   def references(): Set[ModelReference[_]] = {
     this.referenceManager.referenceMap().getAll()
   }
-  
+
   def sessionDisconnected(sessionId: String): Unit = {
     this.referenceManager.sessionDisconnected(sessionId)
   }
-  
+
   def processReferenceEvent(event: ModelReferenceEvent, sessionId: String): Try[Unit] = Try {
+    if (this.validReferenceTypes.isEmpty) {
+      throw new IllegalArgumentException("This value does not allow references")
+    }
     this.referenceManager.handleReferenceEvent(event, sessionId)
   }
 }
