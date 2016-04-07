cb8a2edd9ab2ac4733e45a671236191cdfea4d4d
Cameron <cameron@convergencelabs.com>
2016-04-07 13:04:31 -0700

    Create domain rest call added

--- server-core/src/main/scala/com/convergencelabs/server/datastore/DomainDBController.scala
+++ server-core/src/main/scala/com/convergencelabs/server/datastore/DomainDBController.scala
@@ -1,7 +1,5 @@
 package com.convergencelabs.server.datastore
 
-import com.convergencelabs.server.frontend.rest.DomainFqn
-
 trait DomainDBController {
   def createDomain(): DBConfig
   def deleteDomain(id: String): Unit
--- server-core/src/main/scala/com/convergencelabs/server/frontend/rest/DomainService.scala
+++ server-core/src/main/scala/com/convergencelabs/server/frontend/rest/DomainService.scala
@@ -12,12 +12,17 @@ import com.convergencelabs.server.datastore.DomainStoreActor.ListDomainsResponse
 import scala.util.Try
 import scala.util.Success
 import com.convergencelabs.server.domain.DomainFqn
+import com.convergencelabs.server.datastore.DomainStoreActor.GetDomainRequest
+import com.convergencelabs.server.datastore.DomainStoreActor.GetDomainSuccess
+import com.convergencelabs.server.datastore.DomainStoreActor.GetDomainResponse
+import com.convergencelabs.server.domain.Domain
+import com.convergencelabs.server.datastore.DomainStoreActor.CreateDomainRequest
 
 case class DomainsResponse(ok: Boolean, domains: List[DomainFqn])
-case class DomainResponse(ok: Boolean, domain: Option[DomainInfo])
-case class DomainInfo(namespace: String, domain: String, title: String)
-case class DomainRequest(domainId: String)
+case class DomainResponse(ok: Boolean, domain: Option[Domain])
+case class CreateResponse(ok: Boolean)
 
+case class CreateRequest(namespace: String, domainId: String, displayName: String)
 
 class DomainService(
   private[this] val executionContext: ExecutionContext,
@@ -34,26 +39,41 @@ class DomainService(
         get {
           complete { domainsRequest(userId) }
         }
-      } ~
-        path(Segment) { domainId =>
-          get {
-            complete { domainRequest(domainId) }
+        post {
+          entity(as[CreateRequest]) { request =>
+            complete(createRequest(request, userId))
           }
         }
+      } ~
+        pathPrefix(Segment / Segment) { (namespace, domainId) =>
+          pathEnd {
+            get {
+              complete { domainRequest(namespace, domainId) }
+            }
+          }
+        }
+    }
+  }
+
+  def createRequest(createRequest: CreateRequest, userId: String): Future[CreateResponse] = {
+    val CreateRequest(namespace, domainId, displayName) = createRequest
+    (domainActor ? CreateDomainRequest(namespace, domainId, displayName, userId)).mapTo[Try[Unit]].map {
+      case Success(_) => CreateResponse(true)
+      case _          => CreateResponse(false)
     }
   }
 
   def domainsRequest(userId: String): Future[DomainsResponse] = {
     (domainActor ? ListDomainsRequest(userId)).mapTo[Try[ListDomainsResponse]].map {
       case Success(ListDomainsResponse(domains)) => DomainsResponse(true, (domains map (domain => DomainFqn(domain.domainFqn.namespace, domain.domainFqn.domainId))))
-      case _ => DomainsResponse(false, List())
+      case _                                     => DomainsResponse(false, List())
     }
   }
 
-  def domainRequest(domainId: String): Future[DomainResponse] = {
-    (domainActor ? DomainRequest(domainId)).mapTo[Option[DomainInfo]].map {
-      case Some(d) => DomainResponse(true, Some(d))
-      case None    => DomainResponse(true, None)
+  def domainRequest(namespace: String, domainId: String): Future[DomainResponse] = {
+    (domainActor ? GetDomainRequest(namespace, domainId)).mapTo[Try[GetDomainResponse]].map {
+      case Success(GetDomainSuccess(domain)) => DomainResponse(true, Some(domain))
+      case _                                 => DomainResponse(true, None)
     }
   }
 }
