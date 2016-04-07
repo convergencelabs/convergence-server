e29ddc9fcd2c015902b3eb38a8e34807ad1f636d
Cameron <cameron@convergencelabs.com>
2016-04-07 13:30:40 -0700

    More domain service updates

--- server-core/src/main/scala/com/convergencelabs/server/frontend/rest/DomainService.scala
+++ server-core/src/main/scala/com/convergencelabs/server/frontend/rest/DomainService.scala
@@ -17,10 +17,12 @@ import com.convergencelabs.server.datastore.DomainStoreActor.GetDomainSuccess
 import com.convergencelabs.server.datastore.DomainStoreActor.GetDomainResponse
 import com.convergencelabs.server.domain.Domain
 import com.convergencelabs.server.datastore.DomainStoreActor.CreateDomainRequest
+import com.convergencelabs.server.datastore.DomainStoreActor.DeleteDomainRequest
 
 case class DomainsResponse(ok: Boolean, domains: List[DomainFqn])
 case class DomainResponse(ok: Boolean, domain: Option[Domain])
 case class CreateResponse(ok: Boolean)
+case class DeleteResponse(ok: Boolean)
 
 case class CreateRequest(namespace: String, domainId: String, displayName: String)
 
@@ -37,8 +39,8 @@ class DomainService(
     pathPrefix("domains") {
       pathEnd {
         get {
-          complete { domainsRequest(userId) }
-        }
+          complete(domainsRequest(userId))
+        } ~
         post {
           entity(as[CreateRequest]) { request =>
             complete(createRequest(request, userId))
@@ -48,7 +50,10 @@ class DomainService(
         pathPrefix(Segment / Segment) { (namespace, domainId) =>
           pathEnd {
             get {
-              complete { domainRequest(namespace, domainId) }
+              complete(domainRequest(namespace, domainId))
+            } ~
+            delete {
+              complete(deleteRequest(namespace, domainId))
             }
           }
         }
@@ -73,7 +78,14 @@ class DomainService(
   def domainRequest(namespace: String, domainId: String): Future[DomainResponse] = {
     (domainActor ? GetDomainRequest(namespace, domainId)).mapTo[Try[GetDomainResponse]].map {
       case Success(GetDomainSuccess(domain)) => DomainResponse(true, Some(domain))
-      case _                                 => DomainResponse(true, None)
+      case _                                 => DomainResponse(false, None)
+    }
+  }
+
+  def deleteRequest(namespace: String, domainId: String): Future[DeleteResponse] = {
+    (domainActor ? DeleteDomainRequest(namespace, domainId)).mapTo[Try[Unit]].map {
+      case Success(_) => DeleteResponse(true)
+      case _          => DeleteResponse(false)
     }
   }
 }
