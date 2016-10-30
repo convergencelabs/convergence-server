6addc7af80dd12a9b5185c19a90a7f93be3d69ae
Michael MacFadden <michael@convergencelabs.com>
2016-10-29 20:04:12 -0700

    Renumbered the messages.

--- server-core/src/main/scala/com/convergencelabs/server/frontend/realtime/MessageType.scala
+++ server-core/src/main/scala/com/convergencelabs/server/frontend/realtime/MessageType.scala
@@ -13,95 +13,102 @@ object MessageType extends Enumeration {
   val TokenAuthRequest = 6
   val AuthenticationResponse = 7
 
-  val OpenRealTimeModelRequest = 8
-  val OpenRealTimeModelResponse = 9
+  // Models
+  val OpenRealTimeModelRequest = 100
+  val OpenRealTimeModelResponse = 101
 
-  val CloseRealTimeModelRequest = 10
-  val CloseRealTimeModelResponse = 11
+  val CloseRealTimeModelRequest = 102
+  val CloseRealTimeModelResponse = 103
 
-  val CreateRealTimeModelRequest = 12
-  val CreateRealTimeModelResponse = 13
+  val CreateRealTimeModelRequest = 104
+  val CreateRealTimeModelResponse = 105
 
-  val DeleteRealtimeModelRequest = 14
-  val DeleteRealtimeModelResponse = 15
+  val DeleteRealtimeModelRequest = 106
+  val DeleteRealtimeModelResponse = 107
 
-  val ForceCloseRealTimeModel = 16
+  val ForceCloseRealTimeModel = 108
 
-  val RemoteClientOpenedModel = 17
-  val RemoteClientClosedModel = 18
+  val RemoteClientOpenedModel = 109
+  val RemoteClientClosedModel = 110
 
-  val ModelDataRequest = 19
-  val ModelDataResponse = 20
+  val ModelDataRequest = 111
+  val ModelDataResponse = 112
 
-  val RemoteOperation = 21
+  val RemoteOperation = 113
 
-  val OperationSubmission = 22
-  val OperationAck = 23
+  val OperationSubmission = 114
+  val OperationAck = 115
 
-  val PublishReference = 24
-  val SetReference = 25
-  val ClearReference = 26
-  val UnpublishReference = 27
+  val PublishReference = 116
+  val SetReference = 117
+  val ClearReference = 118
+  val UnpublishReference = 119
 
-  val ReferencePublished = 28
-  val ReferenceSet = 29
-  val ReferenceCleared = 30
-  val ReferenceUnpublished = 31
+  val ReferencePublished = 120
+  val ReferenceSet = 121
+  val ReferenceCleared = 122
+  val ReferenceUnpublished = 123
 
-  val UserLookUpRequest = 50
-  val UserSearchRequest = 51
-  val UserListResponse = 52
+  val ModelsQueryRequest = 124
+  val ModelsQueryResponse = 125
   
-  val ActivityParticipantsRequest = 60
-  val ActivityParticipantsResponse = 61
-
-  val ActivityJoinRequest = 64
-  val ActivityJoinResponse = 65
-
-  val ActivityLeaveRequest = 66
-  val ActivityLeaveResponse = 67
-
-  val ActivitySessionJoined = 68
-  val ActivitySessionLeft = 69
-
-  val ActivityLocalStateSet = 70
-  val ActivityLocalStateRemoved = 71
-  val ActivityLocalStateCleared = 72
-
-  val ActivityRemoteStateSet = 73
-  val ActivityRemoteStateRemoved = 74
-  val ActivityRemoteStateCleared = 75
+  val HistoricalDataRequest = 126
+  val HistoricalDataResponse = 127
   
-  val PresenceSetState = 76
-  val PresenceClearState = 77
-
-  val PresenceStateSet = 78
-  val PresenceStateCleared = 79
-
-  val PresenceAvailabilityChanged = 80
-
-  val PresenceRequest = 81
-  val PresenceResponse = 82
-
-  val PresenceSubscribeRequest = 83
-  val PresenceSubscribeResponse = 84
-  val PresenceUnsubscribe = 85
+  val HistoricalOperationsRequest = 128
+  val HistoricalOperationsResponse = 129
   
-  val JoinRoomRequest = 86
-  val JoinRoomResponse = 87
-  val LeaveRoom = 88
-  val PublishChatMessage = 89
+  // Identity
+  val UserLookUpRequest = 200
+  val UserSearchRequest = 201
+  val UserListResponse = 202
+  
+  // Activity
+  val ActivityParticipantsRequest = 300
+  val ActivityParticipantsResponse = 301
 
-  val UserJoinedRoom = 90
-  val UserLeftRoom = 91
-  val ChatMessagePublished = 92
+  val ActivityJoinRequest = 302
+  val ActivityJoinResponse = 303
+
+  val ActivityLeaveRequest = 304
+  val ActivityLeaveResponse = 305
+
+  val ActivitySessionJoined = 306
+  val ActivitySessionLeft = 307
+
+  val ActivityLocalStateSet = 308
+  val ActivityLocalStateRemoved = 309
+  val ActivityLocalStateCleared = 310
+
+  val ActivityRemoteStateSet = 311
+  val ActivityRemoteStateRemoved = 312
+  val ActivityRemoteStateCleared = 313
   
-  val ModelsQueryRequest = 93
-  val ModelsQueryResponse = 94
+  // Presence
+  val PresenceSetState = 400
+  val PresenceRemoveState = 401
+  val PresenceClearState = 402
+
+  val PresenceStateSet = 403
+  val PresenceStateRemoved = 404
+  val PresenceStateCleared = 405
+
+  val PresenceAvailabilityChanged = 406
+
+  val PresenceRequest = 407
+  val PresenceResponse = 408
+
+  val PresenceSubscribeRequest = 409
+  val PresenceSubscribeResponse = 410
+  val PresenceUnsubscribe = 411
   
-  val HistoricalDataRequest = 95
-  val HistoricalDataResponse = 96
-  
-  val HistoricalOperationsRequest = 97
-  val HistoricalOperationsResponse = 98
+  // Chat
+  val JoinRoomRequest = 500
+  val JoinRoomResponse = 501
+  val LeaveRoom = 502
+  val PublishChatMessage = 503
+
+  val UserJoinedRoom = 504
+  val UserLeftRoom = 505
+  val ChatMessagePublished = 506
 }
