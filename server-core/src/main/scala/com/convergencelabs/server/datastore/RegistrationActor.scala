44d4573b44501c44888b6466efb93897c010b8e9
Michael MacFadden <michael@convergencelabs.com>
2016-12-21 19:05:21 -0700

    Made sending registration emails async.

--- server-core/src/main/scala/com/convergencelabs/server/datastore/RegistrationActor.scala
+++ server-core/src/main/scala/com/convergencelabs/server/datastore/RegistrationActor.scala
@@ -2,20 +2,21 @@ package com.convergencelabs.server.datastore
 
 import java.net.URLEncoder
 
+import scala.concurrent.Future
 import scala.concurrent.duration.DurationInt
 import scala.language.postfixOps
+import scala.util.Failure
 import scala.util.Success
 
 import com.convergencelabs.server.datastore.ConvergenceUserManagerActor.CreateConvergenceUserRequest
-import com.convergencelabs.server.datastore.RegistrationActor.RequestRegistration
 import com.convergencelabs.server.datastore.RegistrationActor.ApproveRegistration
 import com.convergencelabs.server.datastore.RegistrationActor.RegisterUser
 import com.convergencelabs.server.datastore.RegistrationActor.RegistrationInfo
 import com.convergencelabs.server.datastore.RegistrationActor.RegistrationInfoRequest
 import com.convergencelabs.server.datastore.RegistrationActor.RejectRegistration
+import com.convergencelabs.server.datastore.RegistrationActor.RequestRegistration
 import com.convergencelabs.server.util.EmailUtilities
 import com.convergencelabs.templates
-import com.orientechnologies.orient.core.db.OPartitionedDatabasePool
 import com.typesafe.config.Config
 
 import akka.actor.ActorLogging
@@ -24,7 +25,6 @@ import akka.actor.Props
 import akka.actor.actorRef2Scala
 import akka.pattern.ask
 import akka.util.Timeout
-import scala.util.Try
 
 object RegistrationActor {
   def props(dbProvider: DatabaseProvider, userManager: ActorRef): Props = Props(new RegistrationActor(dbProvider, userManager))
@@ -91,69 +91,79 @@ class RegistrationActor private[datastore] (dbProvider: DatabaseProvider, userMa
   def requestRegistration(message: RequestRegistration): Unit = {
     val RequestRegistration(fname, lname, email, reason) = message
     log.debug(s"Processing registration request for email: ${email}")
-    val result = registrationStore.addRegistration(fname, lname, email, reason) flatMap { token =>
+    val result = registrationStore.addRegistration(fname, lname, email, reason) map { token =>
       val bodyContent = templates.email.internal.txt.registrationRequest(token, fname, lname, email, reason, registrationBaseUrl)
       val internalEmail = EmailUtilities.createTextEmail(smtpConfig, bodyContent.toString())
       internalEmail.setSubject(s"Registration Request from ${fname} ${lname}")
       val regEmail = smtpConfig.getString("new-registration-to-address")
       internalEmail.addTo(regEmail)
 
-      Try {
+      Future {
         internalEmail.send()
-        log.debug(s"Sent email notification to: ${regEmail}")
+      } onComplete {
+        case Success(_) =>
+          log.debug(s"Sent email internal registration notification for '${email}' to: ${regEmail}")
+        case Failure(cause) =>
+          log.error(cause, s"Could not send internal registration notification email for: {$email}")
       }
-    } flatMap { _ =>
+      ()
+    } map { _ =>
       val template = templates.email.html.accountRequested(fname)
       val templateTxt = templates.email.txt.accountRequested(fname)
       val newRequestEmail = EmailUtilities.createHtmlEmail(smtpConfig, template, templateTxt.toString())
       newRequestEmail.setSubject(s"${fname}, your account request has been received")
       newRequestEmail.addTo(email)
-      
-      Try {
+
+      Future {
         newRequestEmail.send()
-        log.debug(s"Sent email notification to: ${email}")
-        ()
+      } onComplete {
+        case Success(_) =>
+          log.debug(s"Sent email registration notification to: ${email}")
+        case Failure(cause) =>
+          log.error(cause, s"Could not send registration notification email to: {$email}")
       }
-    } recover {
-      case cause: Exception =>
-        log.error(cause, s"Error processing registration request for '${email}'.")
-        ()
+      ()
     }
-    
+
     reply(result)
   }
 
   def appoveRegistration(message: ApproveRegistration): Unit = {
     val ApproveRegistration(token) = message
-    val resp = registrationStore.approveRegistration(token)
+
+    val resp = for {
+      info <- registrationStore.getRegistrationInfo(token) flatMap {
+        case Some(info) => Success(info)
+        case None => Failure(new IllegalArgumentException("Unable to lookup registration infromation"))
+      }
+      approve <- registrationStore.approveRegistration(token)
+    } yield {
+      val (firstName, lastName, email) = info
+
+      val fnameEncoded = URLEncoder.encode(firstName, "UTF8")
+      val lnameEncoded = URLEncoder.encode(lastName, "UTF8")
+      val emailEncoded = URLEncoder.encode(email, "UTF8")
+      val signupUrl = s"${adminUiServerUrl}signup/${token}"
+      val introTxt = if (firstName != null && firstName.nonEmpty) s"${firstName}, good news!" else "Good news!"
+
+      val templateHtml = templates.email.html.registrationApproved(signupUrl, introTxt)
+      val templateTxt = templates.email.txt.registrationApproved(signupUrl, introTxt)
+
+      val approvalEmail = EmailUtilities.createHtmlEmail(smtpConfig, templateHtml, templateTxt.toString());
+      approvalEmail.setSubject(s"Your Convergence account request has been approved")
+      approvalEmail.addTo(email)
+      
+      Future {
+        approvalEmail.send()
+        log.debug(s"Sent registration approval email to: ${email}")
+      } onFailure {
+        case cause: Exception => 
+          log.error(cause, "Could not send registration approval message to: ${email}")
+      }
+      ()
+    }
 
     reply(resp)
-
-    resp match {
-      case Success(()) => {
-        registrationStore.getRegistrationInfo(token) match {
-          case Success(Some(registration)) => {
-            val (firstName, lastName, email) = registration
-
-            val fnameEncoded = URLEncoder.encode(firstName, "UTF8")
-            val lnameEncoded = URLEncoder.encode(lastName, "UTF8")
-            val emailEncoded = URLEncoder.encode(email, "UTF8")
-            val signupUrl = s"${adminUiServerUrl}signup/${token}"
-            val introTxt = if (firstName != null && firstName.nonEmpty) s"${firstName}, good news!" else "Good news!"
-
-            val templateHtml = templates.email.html.registrationApproved(signupUrl, introTxt)
-            val templateTxt = templates.email.txt.registrationApproved(signupUrl, introTxt)
-
-            val approvalEmail = EmailUtilities.createHtmlEmail(smtpConfig, templateHtml, templateTxt.toString());
-            approvalEmail.setSubject(s"Your Convergence account request has been approved")
-            approvalEmail.addTo(email)
-            approvalEmail.send()
-          }
-          case _ => log.error("Unable to lookup registration to send Email")
-        }
-      }
-      case _ => // Do Nothing, we have already replied with this error
-    }
   }
 
   def getRegistrationInfo(message: RegistrationInfoRequest): Unit = {
