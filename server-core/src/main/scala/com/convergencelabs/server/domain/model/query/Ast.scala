3b951ca9de1ad6e0348148bc545568b962ce88a7
Cameron Meyer <cameron@convergencelabs.com>
2017-01-31 14:21:39 -0800

    Query AST work

--- server-core/src/main/scala/com/convergencelabs/server/domain/model/query/Ast.scala
+++ server-core/src/main/scala/com/convergencelabs/server/domain/model/query/Ast.scala
@@ -1,13 +1,51 @@
 package com.convergencelabs.server.domain.model.query
 
-object Ast {
-  case class SelectStatement(collection: String, where: WhereTerm)
-  case class WhereTerm(field: String, op: EqualityOperator, value: String)
-
-  sealed trait EqualityOperator
-  case object Equals extends EqualityOperator
-  case object GreaterThan extends EqualityOperator
-  case object LessThan extends EqualityOperator
-  case object LessThanOrEqual extends EqualityOperator
-  case object GreaterThanOrEqual extends EqualityOperator
+object Ast {  
+  case class SelectStatement( 
+    collection: String,  
+    where: Option[WhereExpression],  
+    limit: Option[Int],  
+    offset: Option[Int],  
+    orderBy: List[OrderBy])  
+ 
+  sealed trait WhereExpression 
+ 
+  sealed trait LogicalExpression extends WhereExpression 
+  case class And(lhs: WhereExpression, rhs: WhereExpression) extends LogicalExpression 
+  case class Or(lhs: WhereExpression, rhs: WhereExpression) extends LogicalExpression 
+  case class Not(conditional: ConditionalExpression) extends LogicalExpression 
+ 
+  sealed trait ConditionalExpression extends WhereExpression 
+  case class Equals(lhs: Term, rhs: Term) extends ConditionalExpression  
+  case class NotEquals(lhs: Term, rhs: Term) extends ConditionalExpression  
+  case class GreaterThan(lhs: Term, rhs: Term) extends ConditionalExpression  
+  case class LessThan(lhs: Term, rhs: Term) extends ConditionalExpression  
+  case class LessThanOrEqual(lhs: Term, rhs: Term) extends ConditionalExpression  
+  case class GreaterThanOrEqual(lhs: Term, rhs: Term) extends ConditionalExpression  
+  case class In(field: String, value: List[Any]) extends ConditionalExpression 
+  case class Like(field: String, value: String) extends ConditionalExpression 
+  
+ 
+  sealed trait Term 
+ 
+  sealed trait ExpressionValue extends Term 
+  case class DoubleExpressionValue(value: Double) extends Term 
+  case class StringExpressionValue(value: String) extends Term 
+  case class FieldExpressionValue(value: String) extends Term 
+  case class BooleanExpressionValue(value: Boolean) extends Term 
+ 
+ 
+  sealed trait MathematicalOperator extends Term 
+  case class Add(lhs: Term, rhs: Term) extends Term 
+  case class Subract(lhs: Term, rhs: Term) extends Term 
+  case class Divide(lhs: Term, rhs: Term) extends Term 
+  case class Multiply(lhs: Term, rhs: Term) extends Term 
+  case class Mod(lhs: Term, rhs: Term) extends Term 
+ 
+ 
+  case class OrderBy(field: String, direction: Option[OrderByDirection])  
+ 
+  sealed trait OrderByDirection 
+  case object Ascending extends OrderByDirection; 
+  case object Descending extends OrderByDirection; 
 }
--- server-core/src/main/scala/com/convergencelabs/server/domain/model/query/QueryParser.scala
+++ server-core/src/main/scala/com/convergencelabs/server/domain/model/query/QueryParser.scala
@@ -3,48 +3,48 @@ package com.convergencelabs.server.domain.model.query
 import org.parboiled2._
 
 class QueryParser(val input: ParserInput) extends Parser {
-  def InputLine = rule { SelectStatement ~ EOI }
-
-  def SelectStatement = rule { (Select ~ Fields ~ From ~ CollectionToken ~ Where ~ WhereTerms) ~> (Ast.SelectStatement(_, _))}
-
-  def Select = rule { ignoreCase("select") }
-
-  def Fields = rule { optional(WhiteSpace ~ "*") }
-
-  def From = rule { WhiteSpace ~ ignoreCase("from") }
-
-  def WhiteSpace = rule { zeroOrMore(WhiteSpaceChar) }
-
-  def CollectionToken = rule { WhiteSpace ~ capture(oneOrMore(!EndCollection ~ ANY)) ~ EndCollection }
-
-  def EndCollection = rule { WhiteSpaceChar }
-
-  def Where = rule { WhiteSpace ~ ignoreCase("where") ~ WhiteSpace }
-
-  def WhereTerms = rule { WhereTerm }
-
-  def WhereTerm = rule { (FieldName ~ WhiteSpaceChar ~ EqualityOperator ~ WhiteSpace ~ Value) ~> (Ast.WhereTerm(_, _, _)) }
-
-  def FieldName = rule { capture(oneOrMore(!WhiteSpaceChar ~ ANY)) }
-
-  def LogicOperator = rule { And | Or }
-
-  def EqualityOperator = rule { Eq | Gt | Lt | Ge | Le }
-
-  // FIXME we need to define numbers, strings, etc...
-  def Value = rule { capture(oneOrMore(ANY)) }
-
-  def Eq = rule { capture("=") ~> (_ => Ast.Equals) }
-  val Gt = rule { capture(">") ~> (_ => Ast.GreaterThan) }
-  val Lt = rule { capture("<") ~> (_ => Ast.LessThan) }
-  val Ge = rule { capture(">=") ~> (_ => Ast.GreaterThanOrEqual) }
-  val Le = rule { capture("<=") ~> (_ => Ast.LessThanOrEqual) }
-
-  val And = "AND"
-  val Or = "OR"
-  val WhiteSpaceChar = CharPredicate(" \n\r\t\f")
+//  def InputLine = rule { SelectStatement ~ EOI }
+//
+//  def SelectStatement = rule { (Select ~ Fields ~ From ~ CollectionToken ~ Where ~ WhereTerms) ~> (Ast.SelectStatement(_, _))}
+//
+//  def Select = rule { ignoreCase("select") }
+//
+//  def Fields = rule { optional(WhiteSpace ~ "*") }
+//
+//  def From = rule { WhiteSpace ~ ignoreCase("from") }
+//
+//  def WhiteSpace = rule { zeroOrMore(WhiteSpaceChar) }
+//
+//  def CollectionToken = rule { WhiteSpace ~ capture(oneOrMore(!EndCollection ~ ANY)) ~ EndCollection }
+//
+//  def EndCollection = rule { WhiteSpaceChar }
+//
+//  def Where = rule { WhiteSpace ~ ignoreCase("where") ~ WhiteSpace }
+//
+//  def WhereTerms = rule { WhereTerm }
+//
+//  def WhereTerm = rule { (FieldName ~ WhiteSpaceChar ~ EqualityOperator ~ WhiteSpace ~ Value) ~> (Ast.WhereTerm(_, _, _)) }
+//
+//  def FieldName = rule { capture(oneOrMore(!WhiteSpaceChar ~ ANY)) }
+//
+//  def LogicOperator = rule { And | Or }
+//
+//  def EqualityOperator = rule { Eq | Gt | Lt | Ge | Le }
+//
+//  // FIXME we need to define numbers, strings, etc...
+//  def Value = rule { capture(oneOrMore(ANY)) }
+//
+//  def Eq = rule { capture("=") ~> (_ => Ast.Equals) }
+//  val Gt = rule { capture(">") ~> (_ => Ast.GreaterThan) }
+//  val Lt = rule { capture("<") ~> (_ => Ast.LessThan) }
+//  val Ge = rule { capture(">=") ~> (_ => Ast.GreaterThanOrEqual) }
+//  val Le = rule { capture("<=") ~> (_ => Ast.LessThanOrEqual) }
+//
+//  val And = "AND"
+//  val Or = "OR"
+//  val WhiteSpaceChar = CharPredicate(" \n\r\t\f")
 }
 
 object Test extends App {
-  println(new QueryParser("SELECT * FROM files WHERE foo = bar").InputLine.run())
+//  println(new QueryParser("SELECT * FROM files WHERE foo = bar").InputLine.run())
 }
