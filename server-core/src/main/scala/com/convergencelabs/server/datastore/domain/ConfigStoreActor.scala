7db01949c9051a7b6bafb7b98f6f13cc0dc9d3ea
Michael MacFadden <michael@convergencelabs.com>
2017-03-28 12:21:17 -0700

    Added a model snapshot policy config rest service.

--- server-core/src/main/scala/com/convergencelabs/server/datastore/domain/ConfigStoreActor.scala
+++ server-core/src/main/scala/com/convergencelabs/server/datastore/domain/ConfigStoreActor.scala
@@ -2,17 +2,22 @@ package com.convergencelabs.server.datastore
 
 import com.convergencelabs.server.datastore.ConfigStoreActor.GetAnonymousAuth
 import com.convergencelabs.server.datastore.ConfigStoreActor.SetAnonymousAuth
+import com.convergencelabs.server.datastore.ConfigStoreActor.GetModelSnapshotPolicy
+import com.convergencelabs.server.datastore.ConfigStoreActor.SetModelSnapshotPolicy
 import com.convergencelabs.server.datastore.domain.DomainConfigStore
 
 import akka.actor.ActorLogging
 import akka.actor.Props
+import com.convergencelabs.server.domain.ModelSnapshotConfig
 
 object ConfigStoreActor {
   def props(store: DomainConfigStore): Props = Props(new ConfigStoreActor(store))
 
   trait ConfigStoreRequest
-  case class SetAnonymousAuth(enabled: Boolean) extends ConfigStoreRequest
   case object GetAnonymousAuth extends ConfigStoreRequest
+  case class SetAnonymousAuth(enabled: Boolean) extends ConfigStoreRequest
+  case object GetModelSnapshotPolicy extends ConfigStoreRequest
+  case class SetModelSnapshotPolicy(policy: ModelSnapshotConfig) extends ConfigStoreRequest
 }
 
 class ConfigStoreActor private[datastore] (private[this] val store: DomainConfigStore)
@@ -21,6 +26,8 @@ class ConfigStoreActor private[datastore] (private[this] val store: DomainConfig
   def receive: Receive = {
     case GetAnonymousAuth => getAnonymousAuthEnabled()
     case SetAnonymousAuth(enabled) => setAnonymousAuthEnabled(enabled)
+    case GetModelSnapshotPolicy => getModelSnapshotPolicy()
+    case SetModelSnapshotPolicy(policy) => setModelSnapshotPolicy(policy)
     case message: Any => unhandled(message)
   }
 
@@ -30,5 +37,13 @@ class ConfigStoreActor private[datastore] (private[this] val store: DomainConfig
   
   def setAnonymousAuthEnabled(enabled: Boolean): Unit = {
     reply(store.setAnonymousAuthEnabled(enabled))
+  }
+  
+  def getModelSnapshotPolicy(): Unit = {
+    reply(store.getModelSnapshotConfig())
+  }
+  
+  def setModelSnapshotPolicy(policy: ModelSnapshotConfig): Unit = {
+    reply(store.setModelSnapshotConfig(policy))
   }
 }
