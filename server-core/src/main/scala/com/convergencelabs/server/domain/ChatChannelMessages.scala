1516e4fe8a904dd426e5d98f8944fb80cd8b8eba
Cameron Meyer <cameron@convergencelabs.com>
2017-04-27 14:59:02 -0700

    made topic and name change broadcast messages

--- server-core/src/main/scala/com/convergencelabs/server/domain/ChatChannelMessages.scala
+++ server-core/src/main/scala/com/convergencelabs/server/domain/ChatChannelMessages.scala
@@ -41,8 +41,8 @@ object ChatChannelMessages {
   case class UserLeftChannel(channelId: String, eventNumber: Long, timestamp: Instant, username: String) extends ChatChannelBroadcastMessage
   case class UserAddedToChannel(channelId: String, eventNumber: Long, timestamp: Instant, username: String, addedBy: String) extends ChatChannelBroadcastMessage
   case class UserRemovedFromChannel(channelId: String, eventNumber: Long, timestamp: Instant, username: String, removedBy: String) extends ChatChannelBroadcastMessage
-  case class ChannelNameChanged(channelId: String, eventNumber: Long, timestamp: Instant, name: String, setBy: String)
-  case class ChannelTopicChanged(channelId: String, eventNumber: Long, timestamp: Instant, topic: String, setBy: String)
+  case class ChannelNameChanged(channelId: String, eventNumber: Long, timestamp: Instant, name: String, setBy: String) extends ChatChannelBroadcastMessage
+  case class ChannelTopicChanged(channelId: String, eventNumber: Long, timestamp: Instant, topic: String, setBy: String) extends ChatChannelBroadcastMessage
 
   case class ChannelRemoved(channelId: String) extends ChatChannelBroadcastMessage
   
