0ca8ff32a00133851e48179dfde15323b2bddcaf
Michael MacFadden <michael@convergencelabs.com>
2020-04-05 11:35:00 -0500

    Added authorization checks for convergence users REST calls.
    Related to convergencelabs/convergence-project#87

--- src/main/scala/com/convergencelabs/convergence/server/api/rest/ConvergenceUserService.scala
+++ src/main/scala/com/convergencelabs/convergence/server/api/rest/ConvergenceUserService.scala
@@ -16,28 +16,34 @@ import java.time.Instant
 import akka.actor.ActorRef
 import akka.http.scaladsl.marshalling.ToResponseMarshallable.apply
 import akka.http.scaladsl.server.Directive.{addByNameNullaryApply, addDirectiveApply}
-import akka.http.scaladsl.server.Directives.{Segment, _enhanceRouteWithConcatenation, _segmentStringToPathMatcher, _string2NR, as, complete, delete, entity, get, parameters, path, pathEnd, pathPrefix, post, put}
+import akka.http.scaladsl.server.Directives.{Segment, _enhanceRouteWithConcatenation, _segmentStringToPathMatcher, _string2NR, as, authorize, complete, delete, entity, get, parameters, path, pathEnd, pathPrefix, post, put}
 import akka.http.scaladsl.server.Route
 import akka.pattern.ask
 import akka.util.Timeout
 import com.convergencelabs.convergence.server.datastore.convergence.ConvergenceUserManagerActor._
 import com.convergencelabs.convergence.server.datastore.convergence.UserStore.User
-import com.convergencelabs.convergence.server.security.AuthorizationProfile
+import com.convergencelabs.convergence.server.security.{AuthorizationProfile, Permissions}
 
 import scala.concurrent.{ExecutionContext, Future}
 
 object ConvergenceUserService {
+
   case class CreateUserRequest(username: String, firstName: Option[String], lastName: Option[String], displayName: String, email: String, serverRole: String, password: String)
+
   case class UserPublicData(username: String, displayName: String)
-  case class UserData(username: String, firstName: String, lastName: String, displayName: String, email: String, lastLogin: Option[Instant], serverRole: String)
+
+  case class UserData(username: String, firstName: String, lastName: String, displayName: String, email: Option[String], lastLogin: Option[Instant], serverRole: String)
+
   case class PasswordData(password: String)
+
   case class UpdateUserData(firstName: String, lastName: String, displayName: String, email: String, serverRole: String)
+
 }
 
 class ConvergenceUserService(
-  private[this] val executionContext: ExecutionContext,
-  private[this] val userManagerActor: ActorRef,
-  private[this] val defaultTimeout: Timeout) extends JsonSupport {
+                              private[this] val executionContext: ExecutionContext,
+                              private[this] val userManagerActor: ActorRef,
+                              private[this] val defaultTimeout: Timeout) extends JsonSupport {
 
   import ConvergenceUserService._
 
@@ -46,31 +52,40 @@ class ConvergenceUserService(
 
   val route: AuthorizationProfile => Route = { authProfile: AuthorizationProfile =>
     pathPrefix("users") {
+
       pathEnd {
         get {
           parameters("filter".?, "limit".as[Int].?, "offset".as[Int].?) { (filter, limit, offset) =>
-            complete(getUsers(filter, limit, offset))
+            complete(getUsers(filter, limit, offset, authProfile))
           }
         } ~ post {
-          entity(as[CreateUserRequest]) { request =>
-            complete(createConvergenceUser(request))
+          authorize(canManageUsers(authProfile)) {
+            entity(as[CreateUserRequest]) { request =>
+              complete(createConvergenceUser(request))
+            }
           }
         }
       } ~ pathPrefix(Segment) { username =>
         pathEnd {
           get {
-            complete(getUser(username))
+            complete(getUser(username, authProfile))
           } ~ delete {
-            complete(deleteConvergenceUserRequest(username))
+            authorize(canManageUsers(authProfile)) {
+              complete(deleteConvergenceUserRequest(username))
+            }
           } ~ put {
-            entity(as[UpdateUserData]) { request =>
-              complete(updateUser(username, request))
+            authorize(canManageUsers(authProfile)) {
+              entity(as[UpdateUserData]) { request =>
+                complete(updateUser(username, request))
+              }
             }
           }
         } ~ path("password") {
           post {
-            entity(as[PasswordData]) { request =>
-              complete(setUserPassword(username, request))
+            authorize(canManageUsers(authProfile)) {
+              entity(as[PasswordData]) { request =>
+                complete(setUserPassword(username, request))
+              }
             }
           }
         }
@@ -78,22 +93,16 @@ class ConvergenceUserService(
     }
   }
 
-  private[this] def getUsers(filter: Option[String], limit: Option[Int], offset: Option[Int]): Future[RestResponse] = {
+  private[this] def getUsers(filter: Option[String], limit: Option[Int], offset: Option[Int], authorizationProfile: AuthorizationProfile): Future[RestResponse] = {
     (userManagerActor ? GetConvergenceUsers(filter, limit, offset)).mapTo[Set[ConvergenceUserInfo]] map { users =>
-      val publicData = users.map {
-        case ConvergenceUserInfo(User(username, email, firstName, lastName, displayName, lastLogin), globalRole) =>
-          UserData(username, firstName, lastName, displayName, email, lastLogin, globalRole)
-      }
+      val publicData = users.map(mapUser(_, authorizationProfile))
       okResponse(publicData)
     }
   }
 
-  private[this] def getUser(username: String): Future[RestResponse] = {
+  private[this] def getUser(username: String, authorizationProfile: AuthorizationProfile): Future[RestResponse] = {
     (userManagerActor ? GetConvergenceUser(username)).mapTo[Option[ConvergenceUserInfo]] map { user =>
-      val publicUser = user.map {
-        case ConvergenceUserInfo(User(username, email, firstName, lastName, displayName, lastLogin), globalRole) =>
-          UserData(username, firstName, lastName, displayName, email, lastLogin, globalRole)
-      }
+      val publicUser = user.map(mapUser(_, authorizationProfile))
       okResponse(publicUser)
     }
   }
@@ -101,12 +110,12 @@ class ConvergenceUserService(
   private[this] def createConvergenceUser(createRequest: CreateUserRequest): Future[RestResponse] = {
     val CreateUserRequest(username, firstName, lastName, displayName, email, serverRole, password) = createRequest
     val message = CreateConvergenceUserRequest(username, email, firstName.getOrElse(""), lastName.getOrElse(""), displayName, password, serverRole)
-    (userManagerActor ? message) map ( _ => CreatedResponse )
+    (userManagerActor ? message) map (_ => CreatedResponse)
   }
 
   private[this] def deleteConvergenceUserRequest(username: String): Future[RestResponse] = {
     val message = DeleteConvergenceUserRequest(username)
-    (userManagerActor ? message) map ( _ => DeletedResponse )
+    (userManagerActor ? message) map (_ => DeletedResponse)
   }
 
   private[this] def updateUser(username: String, updateData: UpdateUserData): Future[RestResponse] = {
@@ -120,5 +129,18 @@ class ConvergenceUserService(
     val PasswordData(password) = passwordData
     (userManagerActor ? SetPasswordRequest(username, password)).mapTo[Unit] map
       (_ => OkResponse)
+  }
+
+  private[this] def mapUser(user: ConvergenceUserInfo, authProfile: AuthorizationProfile): UserData = {
+    val ConvergenceUserInfo(User(username, email, firstName, lastName, displayName, lastLogin), globalRole) = user
+    if (canManageUsers(authProfile)) {
+      UserData(username, firstName, lastName, displayName, Some(email), lastLogin, globalRole)
+    } else {
+      UserData(username, firstName, lastName, displayName, None, None, globalRole)
+    }
+  }
+
+  private[this] def canManageUsers(authProfile: AuthorizationProfile): Boolean = {
+    authProfile.hasGlobalPermission(Permissions.Global.ManageUsers)
   }
 }
