03912e5c4fd76a4cc1f63e3a7304f8e00f34b104
Michael MacFadden <michael@convergencelabs.com>
2021-05-20 17:06:45 -0500

    Cleaned up the DomainPersistenceStoreSpec.

--- src/test/scala/com/convergencelabs/convergence/server/backend/datastore/domain/DomainPersistenceStoreSpec.scala
+++ src/test/scala/com/convergencelabs/convergence/server/backend/datastore/domain/DomainPersistenceStoreSpec.scala
@@ -1,5 +1,34 @@
+/*
+ * Copyright (c) 2021 - Convergence Labs, Inc.
+ *
+ * This file is part of the Convergence Server, which is released under
+ * the terms of the GNU General Public License version 3 (GPLv3). A copy
+ * of the GPLv3 should have been provided along with this file, typically
+ * located in the "LICENSE" file, which is part of this source code package.
+ * Alternatively, see <https://www.gnu.org/licenses/gpl-3.0.html> for the
+ * full text of the GPLv3 license, if it was not provided.
+ */
 package com.convergencelabs.convergence.server.backend.datastore.domain
 
-class DomainPersistenceStoreSpec {
+import com.convergencelabs.convergence.server.backend.datastore.PersistenceStoreSpec
+import com.convergencelabs.convergence.server.backend.db.DatabaseProvider
+import com.convergencelabs.convergence.server.backend.db.schema.NonRecordingSchemaManager
+import com.convergencelabs.convergence.server.model.DomainId
+import com.convergencelabs.convergence.server.model.server.domain.DomainStatus
+import org.mockito.Mockito
+import org.scalatestplus.mockito.MockitoSugar
 
+import scala.util.Success
+
+class DomainPersistenceStoreSpec
+  extends PersistenceStoreSpec[DomainPersistenceProvider](NonRecordingSchemaManager.SchemaType.Domain)
+    with MockitoSugar {
+
+  protected val domainId: DomainId = DomainId("ns", "domain")
+
+  protected val domainStatusProvider: DomainStatusProvider = mock[DomainStatusProvider]
+  Mockito.when(domainStatusProvider.getDomainStatus).thenReturn(Success(Some(DomainStatus.Online)))
+
+  protected def createStore(dbProvider: DatabaseProvider): DomainPersistenceProvider =
+    new DomainPersistenceProviderImpl(domainId, dbProvider, domainStatusProvider)
 }
